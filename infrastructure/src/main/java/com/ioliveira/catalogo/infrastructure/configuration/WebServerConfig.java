7f53e3b8a015bad296c9bbc9bf009ad721e156cb
Igor Oliveira <igorfoliveira@gmail.com>
2023-02-11 15:10:41 -0300

    springboot e configuracoes

---
+++ infrastructure/src/main/java/com/ioliveira/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.ioliveira.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ioliveira.catalogo")
+public class WebServerConfig {
+}
